45334da73104447026ea0dffa3c8029ba4ed45e5
Clelton <cleltonjm_95@hotmail.com>
2025-04-29 15:21:56 -0300

    feat: implementing interfaces and controllers

--- src/controllers/pets_lister_controller.py
+++ src/controllers/pets_lister_controller.py
@@ -1,8 +1,9 @@
 from typing import Dict, List
 from src.models.sqlite.entities.pets import PetsTable
 from src.models.sqlite.interfaces.pets_repository import PetsRepositoryInterface
+from .interfaces.pets_lister_controller import PetListerControllerInterface
 
-class PetListerController:
+class PetListerController(PetListerControllerInterface):
     def __init__(self, pet_repository: PetsRepositoryInterface) -> None:
         self.__pet_repository = pet_repository
 
